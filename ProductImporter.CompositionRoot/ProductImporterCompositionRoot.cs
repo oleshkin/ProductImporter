fe3958c5eb198f3a5fd6d29851e58f569d42d8fe
Aleksandr Oleshko <aleksandr.oleshko@ericsson.com>
2023-07-19 15:40:10 +0300

    Grouped the DI registrations logically.
    Each DIRegistration is now responsible for registering the dependencies, which belong to the corresponding class.

--- ProductImporter.CompositionRoot/ProductImporterCompositionRoot.cs
+++ ProductImporter.CompositionRoot/ProductImporterCompositionRoot.cs
@@ -5,6 +5,7 @@ using ProductImporter.Logic.Target;
 using ProductImporter.Logic.Transformation;
 using ProductImporter.Logic.Transformations.Util;
 using ProductImporter.Logic.Transformations;
+using ProductImporter.Logic;
 
 namespace ProductImporter.CompositionRoot;
 
@@ -12,26 +13,8 @@ public static class ProductImporterCompositionRoot
 {
     public static IServiceCollection AddProductImporter(this IServiceCollection services)
     {
-        services.AddSingleton<Configuration>();
-
-        services.AddTransient<IPriceParser, PriceParser>();
-        services.AddTransient<IProductFormatter, ProductFormatter>();
-        services.AddTransient<IProductSource, ProductSource>();
-        services.AddTransient<IProductTarget, CsvProductTarget>();
-        services.AddSingleton<IImportStatistics, ImportStatistics>();
-
-        services.AddTransient<ProductImporter.Logic.ProductImporter>();
-
-        services.AddScoped<IProductTransformationContext, ProductTransformationContext>();
-        services.AddScoped<ICurrencyNormalizer, CurrencyNormalizer>();
-        services.AddScoped<INameDecapitaliser, NameDecapitaliser>();
-
-        services.AddScoped<IDateTimeProvider, DateTimeProvider>();
-        services.AddScoped<IReferenceAdder, ReferenceAdder>();
-        services.AddScoped<IReferenceGenerator, ReferenceGenerator>();
-        services.AddSingleton<IProductCounter, ProductCounter>();
-
-        services.AddTransient<IProductTransformer, ProductTransformer>();
+        services.AddProductImporterLogic();
+        services.AddProductTransformations();
 
         return services;
     }
---
+++ ProductImporter.Logic.Transformations/DIRegistrations.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ProductImporter.Logic.Transformations.Util;
+
+namespace ProductImporter.Logic.Transformations;
+
+public static class DIRegistrations
+{
+    public static IServiceCollection AddProductTransformations(this IServiceCollection services)
+    {
+        services.AddScoped<IProductTransformationContext, ProductTransformationContext>();
+        services.AddScoped<ICurrencyNormalizer, CurrencyNormalizer>();
+        services.AddScoped<INameDecapitaliser, NameDecapitaliser>();
+
+        services.AddScoped<IDateTimeProvider, DateTimeProvider>();
+        services.AddScoped<IReferenceAdder, ReferenceAdder>();
+        services.AddScoped<IReferenceGenerator, ReferenceGenerator>();
+        services.AddSingleton<IProductCounter, ProductCounter>();
+        return services;
+    }
+}
