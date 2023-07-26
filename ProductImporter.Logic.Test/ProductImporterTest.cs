7276fce268a8117f14b0c357ab9c364087c4f822
Aleksandr Oleshko <aleksandr.oleshko@ericsson.com>
2023-07-26 18:30:44 +0300

    Added simple test for ProductImporter.

---
+++ ProductImporter.Logic.Test/ProductImporterTest.cs
@@ -0,0 +1,43 @@
+﻿using Moq;
+using ProductImporter.Logic.Shared;
+using ProductImporter.Logic.Source;
+using ProductImporter.Logic.Target;
+using ProductImporter.Logic.Transformation;
+using ProductImporter.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductImporter.Logic.Test;
+
+public class ProductImporterTest
+{
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(4)]
+    public async Task WhenItReadsNProductsFromSource_ThenItWritesNProductsToTarget(int numberOfProducts)
+    {
+        var productSource = new Mock<IProductSource>();
+        var productTransformer = new Mock<IProductTransformer>();
+        var productTarget = new Mock<IProductTarget>();
+        var importStatistics = new Mock<IImportStatistics>();
+
+        var subjectUnderTest = new ProductImporter(productSource.Object, productTransformer.Object, 
+            productTarget.Object, importStatistics.Object);
+
+        var productCounter = 0;
+
+        productSource
+            .Setup(x => x.hasMoreProducts())
+            .Callback(() => productCounter++)
+            .Returns(() => productCounter <= numberOfProducts);
+
+        await subjectUnderTest.RunAsync();
+
+        productTarget
+            .Verify(x => x.AddProduct(It.IsAny<Product>()), Times.Exactly(numberOfProducts));
+    }
+}
